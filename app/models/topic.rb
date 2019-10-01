0c1a457c65b6fd99d528769cd4b22f94c9999c82
wukrit <sukritwalia@gmail.com>
2019-10-01 19:23:54 -0400

    added some source helpers for Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,15 @@ class Topic < ApplicationRecord
     has_many :article_keywords, dependent: :destroy
     has_many :articles, through: :article_keywords
     
+    def self.sources
+        ["CNN", "The New York Times", "BBC News", "USA Today", "Time", "ABC News", "The Wall Street Journal", "Al Jazeera English", "The Washington Post", "Fox News", "The Telegraph", "MSNBC", "Associated Press", "CBS News", "The Hill"] 
+    end
+
+    def sources
+        arr = Article.all.select do |article|
+            article.topics.include?(self)
+        end
+        arr.pluck(:source).uniq
+    end
+
 end
