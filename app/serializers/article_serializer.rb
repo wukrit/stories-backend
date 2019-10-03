5947c728a511efac43e4f5fc76ea091fba8630fc
wukrit <sukritwalia@gmail.com>
2019-10-03 16:52:02 -0400

    Fixed topic serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,7 +1,6 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :source, :author,:url, :img_url, :description, :content, :published_at
-  
-  belongs_to :topic
+
   has_many :likes
   has_many :dislikes
 end
--- app/serializers/likes_serializer.rb
+++ app/serializers/likes_serializer.rb
@@ -1,5 +1,4 @@
 class LikesSerializer < ActiveModel::Serializer
   attributes :id, :article_id, :user_id
-  
-  belongs_to :article
+
 end
