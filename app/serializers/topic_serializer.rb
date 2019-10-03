8130325f7ac09211d81db56b89cc647c2a0871da
wukrit <sukritwalia@gmail.com>
2019-10-03 16:42:49 -0400

    Added like/dislike serializer

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,4 +1,6 @@
 class TopicSerializer < ActiveModel::Serializer
   attributes :id, :title, :value
   has_many :articles
+  has_many :likes, through: :articles
+  has_many :dislikes, through: :articles
 end
