16d73eddbe6baedac80a219360d6befc61e3816a
wukrit <sukritwalia@gmail.com>
2019-10-03 17:00:36 -0400

    Fixed topic serializer?

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
 
     def index
         @topics = Topic.all
-        render json: @topics, inlcude: "**"
+        render json: @topics, include: ["articles", "likes"]
     end
 
     def show
